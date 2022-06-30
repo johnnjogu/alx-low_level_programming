24f724fbfc6abe698d5ac5a8f88950a41fa3e1ec
johnnjogu <johnnjogu99@gmail.com>
2022-06-30 09:11:41 -0700

    intial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void)
+/**
+ * main - the entry for  comment
+ *
+ *return (0) -after the code has print or put
+ */
+
 {
 puts("\"Programming is like building a multilingual puzzle");
 return(0);
