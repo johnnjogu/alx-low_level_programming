ac3dc3743f541be0f4a847ecdae2bba6cb2e6a27
johnnjogu <johnnjogu99@gmail.com>
2022-07-01 08:11:11 -0700

    7. Intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,15 @@
  *
  */
 int main(void)
-{
-	printf("size of a char:%d byte(s)\n", sizeof(char));
-	printf("size of a int:%d byte(s)\n", sizeof(int));
-	printf("size of a long int:%d byte(s)\n", sizeof(long int));
-	printf("size of a long long int:%d byte(s)\n", sizeof(long long int));
-	printf("size of a float:%d byte(s)\n", sizeof(float));
+{	char = a;
+	int = b;
+	long int = c;
+	long long int = d;
+	float = f;
+	printf("size of a char:%d byte(s)\n", (unsigned long)sizeof(char));
+	printf("size of a int:%d byte(s)\n", (unsigned long)sizeof(int));
+	printf("size of a long int:%d byte(s)\n", (unsigned long)sizeof(long int));
+	printf("size of a long long int:%d byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("size of a float:%d byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
