71ba6d5717d7b729f8a0f06f6d13fc1442e1e606
johnnjogu <johnnjogu99@gmail.com>
2022-07-01 06:58:55 -0700

    5. Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,7 @@ int main(void)
  *
  *return (0) -after the code has print or put
  */
-
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
