8b5975568e25ee797b45342375124f7e6c7583dd
johnnjogu <johnnjogu99@gmail.com>
2022-07-05 08:08:56 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -print lowercase alphabet
+ */
+void print_alphabet_x10(void);
+{
+int j;
+int x;
+for (x = 10; x < 10; x++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchat(j);
+}
+_putchar('\n');
+}
+}
