8e26c8b6fd5f62d1d91a6cdfee078ccea26a4124
johnnjogu <johnnjogu99@gmail.com>
2022-07-05 06:46:08 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main - prints putchar
  *
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\');
 return (0);
 }
