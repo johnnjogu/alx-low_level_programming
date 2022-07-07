9e8a16369f2da2f2bd3edd8b878c0619cc829813
johnnjogu <johnnjogu99@gmail.com>
2022-07-07 02:50:22 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - get sum
+ *
+ * @numb1: first numer input
+ * @numb2: second number input
+ *
+ * Return: Always 0 (success)
+ */
+int add(int numb1, int numb2)
+{
+return (numb1 + numb2);
+}
