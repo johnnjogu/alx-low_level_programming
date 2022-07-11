c2ceeea29d4df5185254c9977338d7ef345f23a9
johnnjogu <johnnjogu99@gmail.com>
2022-07-11 07:56:20 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Test the function for positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+ i = 0;
+ postive_or_negative(i);
+
+ return (0);
+}
