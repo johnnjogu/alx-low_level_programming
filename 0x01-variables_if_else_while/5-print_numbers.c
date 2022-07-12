f2ece3c0e6b07a5103a9a7a02826da060d398055
johnnjogu <johnnjogu99@gmail.com>
2022-07-12 05:46:22 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb <= 9; numb++)
+	{
+		printf("%d", numb);
+	}
+
+	printf("\n");
+	return (0);
+}
