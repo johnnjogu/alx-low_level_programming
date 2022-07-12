edc7decda938436e07e03fd9edb4c8be450369b8
johnnjogu <johnnjogu99@gmail.com>
2022-07-12 06:24:47 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char letter;
+	int numb;
+
+	for (numb = '0'; letter <= 'f'; letter++)
+	{
+		putchar(numb);
+	}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+
+	}
+	putchar('\n');
+	return (0);
+}
