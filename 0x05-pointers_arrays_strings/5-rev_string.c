aabbe1d693a6696f6e5ace00374e1bab88e215ae
johnnjogu <johnnjogu99@gmail.com>
2022-07-13 06:57:37 -0700

    fixed

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - a functiom that reverse string
+ * followed by a new line
+ * @s: an input string
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+
+	char aux;
+
+	while (s[len] != '\0')
+
+		len++;
+
+	while (i < len--)
+	{
+		aux = s[i];
+
+		s[i++] = s[len];
+
+		s[len] = aux
+
+	}
+
+}
