1c119692a0e6ab1cd56f1ad2a13cbdbb0d8b2ac2
johnnjogu <johnnjogu99@gmail.com>
2022-07-13 05:54:55 -0700

    Task 3.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,20 +9,17 @@
 
 void _puts(char *str)
 {
-	int count = 0;
+	int i = 0;
 
-	while (count >= 0)
+	while (str[i] != '\0')
 
 	{
-		if (str[count] == '\0')
+		_putchar(str[i]);
 
-		{
-			_putchar('\n');
-			break;
-		}
+		i++;
 
-		_putchar(str[count]);
-		count++;
 	}
+
+	_putchar('\n');
 
 }
