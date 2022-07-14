82a373b5909196030c3448cbf4e65851bf3d8b7f
johnnjogu <johnnjogu99@gmail.com>
2022-07-14 08:26:37 -0700

    fixed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 1 success
+ */
+
+int main(void)
+{
+	fwrite(STDOUT_FILENO, "and that piece if art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int main(void)
 /**
  * main - the entry for  comment
  *
- *return (0) -after the code has print or put
+ *Return: always 0
  */
+
+int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
