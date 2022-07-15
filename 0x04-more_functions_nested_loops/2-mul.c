5989a4190bc54a9aab374d5cc9b8e81aaead9959
johnnjogu <johnnjogu99@gmail.com>
2022-07-14 23:29:38 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integer
+ * @a: single character count
+ * @b: single character count
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = (a * b);
+
+	return (prod);
+
+}
