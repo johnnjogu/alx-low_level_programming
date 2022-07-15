4c25a3a5572e91402dc264ab458aee0ef41f7436
johnnjogu <johnnjogu99@gmail.com>
2022-07-15 11:13:49 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int digit1, digit2, digit3;
+
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= '9'; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 <= '9'; digit3++)
+			{
+				putchar(digit1);
+				putchar(digit2);
+				putchar(digit3);
+
+				if (digit1 < '7')
+
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+
+		}
+	}
+
+	putchar('\n');
+}
