8d3099563e77f53678fc644da9833b1a3aebc0eb
johnnjogu <johnnjogu99@gmail.com>
2022-07-15 01:21:27 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0-6
+ *
+ * Return: always
+ */
+
+void print_numbers(void)
+
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+
+}
