10df6051ee69117ee1f19665d394732bb36b3f52
johnnjogu <johnnjogu99@gmail.com>
2022-07-22 02:03:40 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - this is a function to print string recursivly
+ * @s: string parameter
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+
+		return;
+	}
+
+	_putchar(*s);
+
+	s++;
+
+	_put_recursion(s);
+
+}
